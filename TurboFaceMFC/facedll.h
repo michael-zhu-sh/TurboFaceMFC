9f3f6346b8b302e69caf65ab08e754b7a91d7079
michael-zhu-sh <michael.ai@foxmail.com>
2018-08-21 11:45:32 +0800

    完成建库界面的事件响应。

--- TurboFaceMFC/facedll.h
+++ TurboFaceMFC/facedll.h
@@ -2,6 +2,27 @@
 #include <string>
 #include <vector>
 
+/*
+初始化系统。
+*/
+__declspec(dllexport) int init(
+	const std::string& faceDbPath,
+	const std::string& faceModelPath
+);
+
+/*
+搜索。
+*/
+__declspec(dllexport) int create(
+	const std::string& faceDbPath,
+	const std::string& faceModelPath,
+	const std::string& faceImagesPath,
+	bool append = true
+);
+
+/*
+建库。
+*/
 __declspec(dllexport) size_t search(
 	const std::string& faceImageFile,
 	const std::string& faceDbPath,
